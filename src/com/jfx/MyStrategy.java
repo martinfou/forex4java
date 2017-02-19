f2f3c6144ea6b8a10658f8398fc19af9d53d29f7
martinfournier <martinfou@gmail.com>
2017-02-19 00:04:44 -0500

    simple alway buy strategy

--- src/com/jfx/MyStrategy.java
+++ src/com/jfx/MyStrategy.java
@@ -7,10 +7,16 @@ import com.jfx.strategy.StrategyRunner;
 import java.io.IOException;
 import java.util.Date;
 
+import org.apache.log4j.Logger;
+
 public class MyStrategy extends com.jfx.strategy.Strategy{
+	Logger logger = Logger.getLogger(MyStrategy.class);
+	static Date lastBarTime; 
     public void init(String symbol, int period, StrategyRunner strategyRunner) {
         try {
             System.out.println("init method");
+            logger.info("testing logger");
+            lastBarTime = new Date();
             super.init(symbol, period, strategyRunner);
         } catch (ErrUnknownSymbol e) {
 
@@ -41,11 +47,59 @@ public class MyStrategy extends com.jfx.strategy.Strategy{
 		orderLots, orderMagicNumber, orderModify, orderOpenPrice, orderOpenTime, orderPrint, orderProfit, orderSelect, orderSend,ordersHistoryTotal, orderStopLoss, ordersTotal, orderSwap, orderSymbol, orderTakeProfit, orderTicket, orderType, print,
 		refreshRates, seconds, timeCurrent, year
 		*/
+    	
+    	if(isNewBar())
+    	{
+    		//logger.info("Got a new bar");
+    		try {
+				long ticketNo = orderSend(
+				        symbol,
+				        TradeOperation.OP_BUY,
+				        0.01,
+				        marketInfo(symbol, MarketInfo.MODE_ASK),
+				        10,
+				        marketInfo(symbol, MarketInfo.MODE_ASK)-0.0005, 
+				        marketInfo(symbol, MarketInfo.MODE_ASK)+0.0005, "comment", 0, null
+				);
+			} catch (ErrInvalidFunctionParamvalue | ErrCustomIndicatorError | ErrStringParameterExpected
+					| ErrIntegerParameterExpected | ErrUnknownSymbol | ErrInvalidPriceParam | ErrTradeNotAllowed
+					| ErrLongsNotAllowed | ErrShortsNotAllowed | ErrCommonError | ErrInvalidTradeParameters
+					| ErrServerBusy | ErrOldVersion | ErrNoConnection | ErrTooFrequentRequests | ErrAccountDisabled
+					| ErrInvalidAccount | ErrTradeTimeout | ErrInvalidPrice | ErrInvalidStops | ErrInvalidTradeVolume
+					| ErrMarketClosed | ErrTradeDisabled | ErrNotEnoughMoney | ErrPriceChanged | ErrOffQuotes
+					| ErrRequote | ErrOrderLocked | ErrLongPositionsOnlyAllowed | ErrTooManyRequests | ErrTradeTimeout2
+					| ErrTradeTimeout3 | ErrTradeTimeout4 | ErrTradeModifyDenied | ErrTradeContextBusy
+					| ErrTradeExpirationDenied | ErrTradeTooManyOrders e) {
+				logger.error(e.getMessage());
+			}	
+    	}
+    	
     	try{
-            System.out.println("Time " + timeCurrent() + " " + iTime(getSymbol(), getTimeframe(), 0));
-        }
+ 
+    	}
         catch(Exception e){
         	System.out.println(e.getMessage());
         }
     }
+    
+    private boolean isNewBar(){
+    	try {
+    		
+			Date iTime = iTime(getSymbol(), getTimeframe(), 0);
+			if(!lastBarTime.equals(iTime)){
+				//System.out.println(lastBarTime.toString());
+				lastBarTime=iTime;
+				return true;
+			}
+		} catch (ErrHistoryWillUpdated e) {
+			logger.info("testing logger");
+			e.printStackTrace();
+		} catch (ErrUnknownSymbol e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return false;
+    	
+    }
+    
 }
